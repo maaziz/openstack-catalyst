b05a1518e4d7ce71dcc710fb3835f06946b27862
chnm-kulkarni <chnm.kulkarni@gmail.com>
2012-07-27 11:13:33 +0530

    Some extra fine tuning

--- quantum/quantum/plugins/cisco/catalyst/cisco_catalyst_snippets.py
+++ quantum/quantum/plugins/cisco/catalyst/cisco_catalyst_snippets.py
@@ -1,3 +1,7 @@
+# Subinterface created on vlan 40 with ip address 192.0.2.1
+# and netmask 255.255.255.0
+# - Chinmay
 SUBINTERFACE_CREATE = """
-ios_config "int g1/4.1" "encapsulation dot1q 40" " ip address 192.0.2.1 255.255.255.0" "no shut"
+ios_config "int g1/4.1" "encapsulation dot1q 40" " ip address 192.0.2.1
+255.255.255.0" "no shut"
 """
